b8c04a86afe6b4d78fc54701ee3c843e673b909a
immadisetty-jaya-surya <suryaimmadisetty5252@gmail.com>
2024-07-05 22:59:36 +0530

    setting interface changed

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -29,117 +29,162 @@ const Settings = ({open,onClose,onSave,onCancel,dispatch, clearAllData,settings}
   return (
     <div>
       <Dialog open={open} onClose={onClose} maxWidth='md' fullWidth>
-        <DialogTitle>Settings</DialogTitle>
-        <DialogContent>
-          <FormControl fullWidth margin="normal">
-            <InputLabel>{en.Settings.Temperature.Title}</InputLabel>
-            <InputLabel>{en.Settings.Temperature.SubTitle}</InputLabel>
-            <TextField 
+        <DialogTitle className="flex justify-center">Settings</DialogTitle>
+        
+        <div className="p-4 border border-black border-opacity-25 rounded-lg mx-4 bg-gray-100">
+          <div className="mb-4 flex items-center justify-between">
+            <div className="mr-4">
+              <label className="block text-lg">{en.Settings.Temperature.Title}</label>
+              <label className="block text-sm text-gray-600">{en.Settings.Temperature.SubTitle}</label>
+            </div>
+            <input 
+              className="p-2 border rounded w-1/3"
               type="number" 
               name="temperature" 
-              value={settings.Temperature}
+              value={settings.temperature}
               onChange={handleChange}
-              inputProps={{step: 0.1, min:0,max:1}}
-              />
-          </FormControl>
-          <FormControl fullWidth margin="normal">
-            <InputLabel>{en.Settings.TopP.Title}</InputLabel>
-            <InputLabel>{en.Settings.TopP.SubTitle}</InputLabel>
-            <TextField
+              step="0.1"
+              min="0"
+              max="1"
+            />
+          </div>
+          <div className="mb-4 flex items-center justify-between">
+            <div className="mr-4">
+              <label className="block text-lg">{en.Settings.TopP.Title}</label>
+              <label className="block text-sm text-gray-600">{en.Settings.TopP.SubTitle}</label>
+            </div>
+            <input 
+              className="p-2 border rounded w-1/3"
               type="number" 
-              name="maxTokens" 
+              name="topP" 
               value={settings.topP}
               onChange={handleChange}
-              inputProps={{step: 0.1, min:0, max:1}}
+              step="0.1"
+              min="0"
+              max="1"
             />
-          </FormControl>
-          <FormControl fullWidth margin="normal">
-            <InputLabel>{en.Settings.MaxTokens.Title}</InputLabel>
-            <InputLabel>{en.Settings.MaxTokens.SubTitle}</InputLabel>
-            <TextField
+          </div>
+
+          <div className="mb-4 flex items-center justify-between">
+            <div className="mr-4">
+              <label className="block text-lg">{en.Settings.MaxTokens.Title}</label>
+              <label className="block text-sm text-gray-600">{en.Settings.MaxTokens.SubTitle}</label>
+            </div>
+            <input 
+              className="p-2 border rounded w-1/3"
               type="number" 
               name="maxTokens" 
               value={settings.maxTokens}
               onChange={handleChange}
-              inputProps={{step: 1, min:1}}
+              step="1"
+              min="1"
             />
-          </FormControl>
-          <FormControl fullWidth margin="normal">
-            <InputLabel>{en.Settings.PresencePenalty.Title}</InputLabel>
-            <InputLabel>{en.Settings.PresencePenalty.SubTitle}</InputLabel>
-            <TextField
+          </div>
+
+          <div className="mb-4 flex items-center justify-between">
+            <div className="mr-4">
+              <label className="block text-lg">{en.Settings.PresencePenalty.Title}</label>
+              <label className="block text-sm text-gray-600">{en.Settings.PresencePenalty.SubTitle}</label>
+            </div>
+            <input 
+              className="p-2 border rounded w-1/3"
               type="number" 
               name="presencePenalty" 
               value={settings.presencePenalty}
               onChange={handleChange}
-              inputProps={{step: 0.1, min:0, max:2}}
-              />
-          </FormControl>
-          <FormControl fullWidth margin="normal">
-            <InputLabel>{en.Settings.FrequencyPenalty.Title}</InputLabel>
-            <InputLabel>{en.Settings.FrequencyPenalty.SubTitle}</InputLabel>
-            <TextField
+              step="0.1"
+              min="0"
+              max="2"
+            />
+          </div>
+
+          <div className="mb-4 flex items-center justify-between">
+            <div className="mr-4">
+              <label className="block text-lg">{en.Settings.FrequencyPenalty.Title}</label>
+              <label className="block text-sm text-gray-600">{en.Settings.FrequencyPenalty.SubTitle}</label>
+            </div>
+            <input 
+              className="p-2 border rounded w-1/3"
               type="number" 
               name="frequencyPenalty" 
               value={settings.frequencyPenalty}
               onChange={handleChange}
-              inputProps={{step: 0.1, min:0, max:2}}
-              />
-          </FormControl>
-          <FormControl component='fieldset' fullWidth margin="normal">
-            <Typography >
-              <InputLabel>{en.Settings.InjectSystemPrompts.Title}</InputLabel>
-              <InputLabel>{en.Settings.InjectSystemPrompts.SubTitle}</InputLabel>
-            </Typography>
-            <FormControlLabel
-              labelPlacement="start"
-              control={
-                <Switch
+              step="0.1"
+              min="0"
+              max="2"
+            />
+          </div>
+
+          <div className="mb-4 flex items-center justify-between">
+            <div>
+              <label className="block text-lg">{en.Settings.InjectSystemPrompts.Title}</label>
+              <label className="block text-sm text-gray-600">{en.Settings.InjectSystemPrompts.SubTitle}</label>
+            </div>
+            <div className="flex items-center">
+              <span className="mr-2">Off</span>
+              <input 
+                className="mr-2"
+                type="checkbox" 
+                name="injectSystemPrompts" 
                 checked={settings.injectSystemPrompts}
                 onChange={handleChange}
-                name="injectSystemPrompts"
-                />
-              }
-            />
-          </FormControl>
-          <FormControl fullWidth margin="normal">
-            <InputLabel>{en.Settings.InputTemplate.Title}</InputLabel>
-            <InputLabel>{en.Settings.InputTemplate.SubTitle}</InputLabel>
-            <TextField
+              />
+              <span>On</span>
+            </div>
+          </div>
+
+          <div className="mb-4 flex items-center justify-between">
+            <div className="mr-4">
+              <label className="block text-lg">{en.Settings.InputTemplate.Title}</label>
+              <label className="block text-sm text-gray-600">{en.Settings.InputTemplate.SubTitle}</label>
+            </div>
+            <input 
+              className="p-2 border rounded w-1/3"
+              type="text"
               name="inputTemplate" 
               value={settings.inputTemplate}
               onChange={handleChange}
-              />
-          </FormControl>
-          <FormControl fullWidth margin="normal">
-            <InputLabel>{en.Settings.HistoryCount.Title}</InputLabel>
-            <InputLabel>{en.Settings.HistoryCount.SubTitle}</InputLabel>
-            <TextField
+            />
+          </div>
+
+          <div className="mb-4 flex items-center justify-between">
+            <div className="mr-4">
+              <label className="block text-lg">{en.Settings.HistoryCount.Title}</label>
+              <label className="block text-sm text-gray-600">{en.Settings.HistoryCount.SubTitle}</label>
+            </div>
+            <input 
+              className="p-2 border rounded w-1/3"
               type="number" 
               name="attachedMessagesCount" 
               value={settings.attachedMessagesCount}
               onChange={handleChange}
-              inputProps={{step:1,min:1,max:64}}
-              />
-          </FormControl>
-          <FormControl fullWidth margin="normal">
-            <InputLabel>{en.Settings.CompressThreshold.Title}</InputLabel>
-            <InputLabel>{en.Settings.CompressThreshold.SubTitle}</InputLabel>
-            <TextField
+              step="1"
+              min="1"
+              max="64"
+            />
+          </div>
+
+          <div className="mb-4 flex items-center justify-between">
+            <div className="mr-4">
+              <label className="block text-lg">{en.Settings.CompressThreshold.Title}</label>
+              <label className="block text-sm text-gray-600">{en.Settings.CompressThreshold.SubTitle}</label>
+            </div>
+            <input 
+              className="p-2 border rounded w-1/3"
               type="number" 
               name="historyCompressionThreshold" 
               value={settings.historyCompressionThreshold}
               onChange={handleChange}
-              inputProps={{step:1,min:1}}
+              step="1"
+              min="1"
             />
-          </FormControl>
-        </DialogContent> 
-        <div className="mt-4">
+          </div>
+        </div>
+        <div className="flex mt-4 pb-6 w-96 justify-between mx-auto">
           <Button
             variant="contained"
             color="secondary"
             onClick={handleReset}
-            className="mb-2"
           >
             Reset All Settings
           </Button>
